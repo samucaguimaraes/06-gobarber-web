e5ef96f006de748e42ab097bd1da7886b23fbd1b
Samuel Gomes Guimaraes <samuca.guimaraes@gmail.com>
2020-08-11 00:19:34 -0300

    - Estrutura limpa

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import SignIn from './pages/index';
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
+export default App;

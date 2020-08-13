ccf0cef4d0019d9cf48f7ef25edc63a04c2756ba
Samuel Gomes Guimaraes <samuca.guimaraes@gmail.com>
2020-08-12 21:45:24 -0300

    - tela de cadastro singUp e configuração do unform

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import SignIn from './pages/Signin/index';
+import SignUp from './pages/SignUp/index';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    {/* <SignIn /> */}
+    <SignUp />
     <GlobalStyle />
   </>
 );
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,18 +1,34 @@
-import React, { InputHTMLAttributes } from 'react';
-import {IconBaseProps} from 'react-icons';
+import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
+import { IconBaseProps } from 'react-icons';
 import { Container } from './styles';
+import { useField } from '@unform/core';
 
 /** Propriedades que o Input vai receber com InputHTMLAttributes */
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
-  icon: React.ComponentType<IconBaseProps>;
+  icon?: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ icon: Icon, ...rest}) => (
-  <Container>
-    {Icon && <Icon size={20} />}
-    <input {...rest} />
-  </Container>
-);
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  /** Utilizando o unform para formulários e persistencia de inputs */
+  const inputRef = useRef(null);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  useEffect( () => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  },[fieldName, registerField]);
+
+
+  return (
+    <Container>
+      {Icon && <Icon size={20} />}
+      <input ref={inputRef} {...rest} />
+    </Container>
+  );
+};
 
 export default Input;

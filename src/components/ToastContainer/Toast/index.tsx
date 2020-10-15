99262a740b247724ca1e737a54f6d118f576b9f3
Samuel Guimarães <samuca.guimaraes@gmail.com>
2020-10-14 22:14:59 -0300

    Toast personalizado por info || success || error

--- src/components/ToastContainer/Toast/index.tsx
+++ src/components/ToastContainer/Toast/index.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Container } from './styles';
 import { ToastMessage, useToast } from '../../../hooks/Toast';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
+import { FiInfo, FiCheckCircle, FiAlertCircle, FiXCircle } from 'react-icons/fi';
 
 interface ToastProps{
   message: ToastMessage;
 }
 
+const icons = {
+  info: <FiInfo size={24} />,
+  error: <FiAlertCircle size={24} />,
+  success: <FiCheckCircle size={24} />,
+}
+
 const Toast: React.FC<ToastProps> = ({ message }) => {
   const { removeToast } = useToast();
 
+  //*** Usando o SetTimeout para executar após a criação do Toast o removeToast()
+  //** Caso o mesmo seja fechado antes é executado o clearTimeout
+  useEffect(()=>{
+    const  timer = setTimeout(() => {
+      removeToast(message.id);
+    }, 3000);
+
+    return  () => {
+      clearTimeout(timer);
+    }
+  },[message.id, removeToast])
+
   return (
     <Container type={message.type} hasDescription={!!message.description}>
-      <FiAlertCircle size={20} />
-
+      {icons[message.type || 'info']}
       <div>
         <strong>{message.title}</strong>
         {message.description && <p>{message.description}</p>}

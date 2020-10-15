c18ea0e97f9aac221ce719f2792df6b683095c95
Samuel Guimarães <samuca.guimaraes@gmail.com>
2020-10-14 21:57:18 -0300

    Create a sub component for tost in th ToastContainer

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,36 +1,26 @@
 import React from 'react';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
-/** Importando interface do hook do toast */
-import { ToastMessage, useToast } from '../../hooks/Toast';
 
-import {Container,Toast} from './styles';
+
+import Toast from './Toast';
+
+/** Importando interface do hook do toast */
+import { ToastMessage } from '../../hooks/Toast';
+import {Container} from './styles';
 
 interface ToastContainerProps {
   messages: ToastMessage[];
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
-  const { removeToast } = useToast();
+
 
   return (
     <Container>
       {messages.map((message) => (
         <Toast
           key={message.id}
-          type={message.type}
-          hasDescription={!!message.description}
-        >
-          <FiAlertCircle size={20} />
-
-          <div>
-            <strong>{message.title}</strong>
-            {message.description && <p>{message.description}</p>}
-          </div>
-
-          <button onClick={() => removeToast(message.id)} type="button">
-            <FiXCircle size={18} />
-          </button>
-        </Toast>
+          message={message}
+        />
       ))}
     </Container>
   );
